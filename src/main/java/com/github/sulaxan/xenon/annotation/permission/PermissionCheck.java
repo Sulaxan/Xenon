e1f2d31fe2c20aacfa57fd18a83e7c017a4a3f88
Encast <encastcore@gmail.com>
2019-09-07 22:05:48 -0400

    Add annotation retention

--- src/main/java/com/github/sulaxan/xenon/annotation/permission/PermissionCheck.java
+++ src/main/java/com/github/sulaxan/xenon/annotation/permission/PermissionCheck.java
@@ -1,5 +1,12 @@
 package com.github.sulaxan.xenon.annotation.permission;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface PermissionCheck {
 
     PermissionScope scope() default PermissionScope.SUB_COMMAND;
--- src/main/java/com/github/sulaxan/xenon/data/AnnotationParser.java
+++ src/main/java/com/github/sulaxan/xenon/data/AnnotationParser.java
@@ -1,12 +1,27 @@
 package com.github.sulaxan.xenon.data;
 
+import com.github.sulaxan.xenon.annotation.Flag;
+import com.github.sulaxan.xenon.data.mapping.FlagMapping;
+
+import java.lang.reflect.Field;
+
 public class AnnotationParser {
 
     // Prevent initialization
     private AnnotationParser() {
     }
 
-    public static void parseFlag() {
+    public static FlagMapping parseFlag(Field field) {
+        try {
+            if(field.isAnnotationPresent(Flag.class)) {
 
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }

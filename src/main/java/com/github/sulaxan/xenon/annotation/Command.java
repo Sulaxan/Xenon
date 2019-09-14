802b4e071ee77321927692604e39af773032d410
Encast <encastcore@gmail.com>
2019-09-13 23:37:54 -0400

    Add more documentation

--- src/main/java/com/github/sulaxan/xenon/annotation/Command.java
+++ src/main/java/com/github/sulaxan/xenon/annotation/Command.java
@@ -5,11 +5,28 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Represents that the class declaring this annotation is a command
+ * class. All command classes must have this annotation, otherwise
+ * the runtime parser will discard the class as a normal class.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Command {
 
+    /**
+     * The names of the command. Names must be of size 1 or greater,
+     * and any further specified names will be treated as aliases.
+     *
+     * @return All possible names used to call the command.
+     */
     String[] names();
 
+    /**
+     * The description of the command. This value is optional, default
+     * implementation will return an empty string.
+     *
+     * @return The description of the command.
+     */
     String desc() default "";
 }
--- src/main/java/com/github/sulaxan/xenon/annotation/Option.java
+++ src/main/java/com/github/sulaxan/xenon/annotation/Option.java
@@ -5,15 +5,36 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Represents an option a command may have. All options start with "-"
+ * for the short option and "--" for the long option.
+ *
+ * e.g., -i, --interactive
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Option {
 
+    /**
+     * @return The short option value
+     */
     String value();
 
+    /**
+     * The long option value, usually complimenting the short option
+     * value.
+     *
+     * @return The long option value.
+     */
     String longOption() default "";
 
+    /**
+     * @return Whether this option is required. False by default.
+     */
     boolean required() default false;
 
+    /**
+     * @return The description of the option. Empty string by default.
+     */
     String desc() default "";
 }

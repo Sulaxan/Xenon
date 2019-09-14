b5a8119bd0bc8290346190d797874075fd43a40d
Encast <encastcore@gmail.com>
2019-09-13 23:29:31 -0400

    Remove unneeded annotation type

--- src/main/java/com/github/sulaxan/xenon/annotation/permission/PermissionScope.java
+++ src/main/java/com/github/sulaxan/xenon/annotation/permission/PermissionScope.java
@@ -1,7 +1,21 @@
 package com.github.sulaxan.xenon.annotation.permission;
 
+/**
+ * Represents the scope of permission methods.
+ *
+ * @see PermissionCheck for further insights.
+ */
 public enum PermissionScope {
 
+    /**
+     * Indicates the scope of the permission to check the root
+     * command.
+     */
     ROOT,
+
+    /**
+     * Indicates the scope of the permission to check specific
+     * sub commands.
+     */
     SUB_COMMAND
 }
